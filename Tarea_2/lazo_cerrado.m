f6ca1e827ab5ec44b386cb0e77965d3b08e38969
TomasIbaceta <33475042+TomasIbaceta@users.noreply.github.com>
2020-10-13 00:18:32 -0300

    Quality of life
    se agregan las perturbaciones de lazo cerrado, se cambian unos scopes de lugar, se cambia el resolucion del scope, blablabla.

--- Tarea_2/lazo_cerrado.m
+++ Tarea_2/lazo_cerrado.m
@@ -10,18 +10,23 @@ alfa = 2; %coeficiente
 
 %------ DATOS CONTROLADOR ------
 
-K_c = 0.0095448; %Tanteado con SISOTOOL
-c = -12.22; %Tanteado con SISOTOOL
+K_c = 0.0058319; %Tanteado con SISOTOOL
+c = -20; %Tanteado con SISOTOOL
 p = 0; %MATEMATICAMENTE CORRECTO
 
 %------ Datos de entradas -----
 
-r = 2;
+r = 2; %referencia constante
+D_a = 0.1; %perturbacion salida ->amplitud
+D_w = 5; %perturbacion salida ->frecuencia
+D_ph = pi/2; %perturbacion salida ->fase
+
 
 %----- ANALISIS SISO-TOOL DE LA PLANTA -------
+
 s = tf ('s');
 C = K_c * (s-c)/ (s-p)
 G_o = K_g * omega_n^2 * ((-1)*alfa*s + 1) / (s^2 + 2*xi*omega_n*s + omega_n^2)
-sisotool(G_o, C)
-
+%sisotool(G_o, C)
+%open("controlador_lazo_cerrado.slx")
 
