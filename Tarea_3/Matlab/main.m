dadbfe3fb3199dd49a69afe472cbcff74c405eba
TomasIbaceta <33475042+TomasIbaceta@users.noreply.github.com>
2020-11-18 15:35:23 -0300

    corregido funcional
    tiene ganancia brigida pero de que funciona funciona.

--- Tarea_3/Matlab/main.m
+++ Tarea_3/Matlab/main.m
@@ -1,10 +1,9 @@
 %------------------------ TAREA 3 -----------------------------
 %NOTA: Es como se marcan zonas de interés directo para quien revise.
-
 clc;
 clear;
 s = tf ('s');
-
+x = sym('x');
 %------ DATOS PLANTA -------
 m = 0.7; %masa [kg]
 l = 0.75; %largo [m]
@@ -12,7 +11,9 @@ cr = 0.05; %constante de roce viscoso [N m s/rad]
 J = m*(l^2); %momento de inercia del pendulo
 g = 9.81; %Aceleracion de gravedad: [m/s^2]
 
-G0 = 1/(J*s^2+ cr*s - m*g*l); %modelo linealizado en (0,pi)
+%G0 = 1/(J*s^2+ s*(cr) - (m*g*l)); %modelo linealizado en (0,pi)
+G0 = (1/J)/(s^2+ s*(cr/J) - (m*g*l/J));
+%esto funciona para K aprox 14
 
 %------ CONTROLADOR proporcional------
 K_p = 5.1503; 
@@ -21,12 +22,35 @@ Cp = K_p;
 
 %------ Controlador PD ---------
 %cancelación del cero: se determina el cero tal que cancele el polo estable
-x = sym('x');
-H = J*x^2+ cr*x - m*g*l;
+
+H = (J*x^2+ x*(cr) - (m*g*l));
 eq = solve(H);
 c = -1*double(eq(1)); %-1 porque la raiz de la cuadrática da la vuelta del signo que se necesita.
 
-K_pd = 40;
+K_pd = 14;
 p = 10;
-Cpd = K_pd * (s+c) / (s+p);
-%sisotool(G0, Cpd)
+Cpd = K_pd * (s+c) / (s+p)
+%sisotool(G0, Cpd) %NOTA: De aquí se obtienen las imágenes para (4.2)
+
+%--------- Agregando el actuador -------
+T_d = 0.1;
+T_e = 0.1;
+K_a = 0.1;
+A = K_a * exp(-1*T_d*s) / (T_e*s+1);
+%A = K_a / (T_e*s+1);
+Gdelta = 1-A;
+G = A*G0; %planta real linealizada en (0,pi)
+
+T0 = (G0*Cpd)/(1+G0*Cpd);
+%sisotool(G,Cpd)
+%bode(T0*Gdelta)
+
+%--------- Corrigiendo el controlador
+c2 = 3.681;
+p2 = 30;
+K_pd_mod = 425;
+
+Cpd_mod = K_pd_mod*(s+c2)/ ...
+                   (s+p2);
+sisotool(G,Cpd_mod)
+
